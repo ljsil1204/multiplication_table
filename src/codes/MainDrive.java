9e69315cdb82264f0ed08bcf6ff846d71131ef17
이진실 <ljsil1204@naver.com>
2022-01-19 15:23:16 +0900

    프로젝트 기본셋팅

---
+++ src/codes/MainDrive.java
@@ -0,0 +1,19 @@
+package codes;
+
+public class MainDrive {
+
+	public static void main(String[] args) {
+		
+//		[응용문제 - 과제] 구구단 2~9단 출력
+		
+		for(int i=2; i<=9; i++) {
+			
+			for(int j=1; j<=9; j++) {
+				System.out.println(i +"x" + j + "=" + i*j);
+			}
+			
+		}
+		
+	}
+	
+}
